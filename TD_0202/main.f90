34f577fde8f330341e79932ebeb632d044d0ba89
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2023-09-22 18:08:38 +0200

    Update main.f90

--- TD_0202/main.f90
+++ TD_0202/main.f90
@@ -10,17 +10,21 @@
 
 program main
     implicit none
-    real :: x,a,b
+    real :: x,a,b,ans
     a = 8.813
     b = 721.5
-    read*, x
+    x=31
+    
+    do while (x<0 .or. x>30)
+    print*, "Pick a number from 0 to 30 : "
+        read*, x
+    end do
     
     cond : if (x>=0 .and. x<5) then
-        write(*,*) x**2 + b*x - 4*exp(x+a)
+        write(*,*) "F(x) = ", x**2 + b*x - 4*exp(x+a)
         else if (x>=5 .and. x<=15) then
-        write(*,*) (((sqrt(x+a))**3)/(1+4*a*exp(a)))
+        write(*,*) "F(x) = ", (((sqrt(x+a))**3)/(1+4*a*exp(a)))
         elseif  (x>15 .and. x<=30) then
-        write(*,*) ((x+(3/2))*sqrt((a*(x**2) + b*sqrt(x))+5*a*x)/(3*(x**2) +4*b))
+        write(*,*) "F(x) = ", ((x+(3/2))*sqrt((a*(x**2) + b*sqrt(x))+5*a*x)/(3*(x**2) +4*b))
     endif cond
-    
 end program main
