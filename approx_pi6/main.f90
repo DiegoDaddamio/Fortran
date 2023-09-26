65a1b8f23ec792b360b4c91f03a352b11d218718
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2023-09-26 12:37:22 +0200

    Add files via upload

---
+++ approx_pi6/main.f90
@@ -0,0 +1,18 @@
+! ---------------------------------
+! µFortran auto generated program.
+! Created for : ddaddami
+! Date : Tue Sep 26 11:13:28 CEST 2023
+! ---------------------------------
+program main
+    implicit none
+    real :: sum
+    integer :: i
+    sum = 0
+    
+    do i =1,1000000000
+        sum = sum + 1.0/ (real(i)**2)
+    enddo
+    
+    print*, sum
+    
+end program main
---
+++ upper_or_lower/main.f90
@@ -0,0 +1,37 @@
+! ---------------------------------
+! µFortran auto generated program.
+! Created for : ddaddami
+! Date : Tue Sep 26 12:06:23 CEST 2023
+! ---------------------------------
+program main
+    implicit none
+    integer :: mystery, guess
+    integer,dimension(8) :: data_time
+    
+    call date_and_time(VALUES=data_time)
+    
+    mystery = int(data_time(8))
+     
+    mystery = mod(mystery*32457,101)
+    
+    guess = 101
+    
+    
+        do while(guess<0 .or. guess>100)
+            print*, "Entrez un nombre entre 0 et 100 : "
+            read*, guess
+        enddo
+    
+        if guess == mystery then
+            print*, "Bravo !"
+        elseif guess > mystery then
+            print*, 
+        elseif guess < mystery then
+            print*, 
+        endif
+          
+    
+    
+    print*, mystery
+   
+end program main
