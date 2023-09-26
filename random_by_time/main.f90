d847941766d40bf7bea63149fbce56eadf5e8437
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2023-09-26 18:27:41 +0200

    Add files via upload

---
+++ random_by_time/main.f90
@@ -0,0 +1,27 @@
+! ---------------------------------
+! µFortran auto generated program.
+! Created for : Diego
+! Date : Tue Sep 26 17:16:42 CEST 2023
+! ---------------------------------
+program main
+    implicit none
+    integer :: random_number, i, max_, size
+    integer,dimension(8) :: data_time ! read the time of the computer
+    max_ = 101 ! 101 not include so [0,100]
+    size = 10 ! number of random integer
+    
+    do i=1, size ! how many random integer that we will have
+    
+        call date_and_time(VALUES=data_time) ! read the time in the computer
+    
+        random_number = int(data_time(8)) ! select the ms
+    
+        random_number = mod(random_number*32457*i,567) ! try to randomise with the module
+        random_number = mod(random_number*25546*i,249) ! try to randomise with the module
+        random_number = mod(random_number*65477*i,max_) ! return it from 0 to the max_
+        
+        print*, random_number
+    
+    enddo
+
+end program main
