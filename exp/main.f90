5fa8fe6049452bdd31c3c1dd0585442ae40d162d
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2023-09-26 18:29:19 +0200

    Delete exp directory

--- exp/main.f90
+++
@@ -1,14 +0,0 @@
-! ---------------------------------
-! µFortran auto generated program.
-! Created for : ddaddami
-! Date : Fri Sep 22 09:10:31 CEST 2023
-! ---------------------------------
-program main
-    implicit none
-
-   integer:: i
-   
-   do i = 1, 17000
-    
-   enddo
-end program main
