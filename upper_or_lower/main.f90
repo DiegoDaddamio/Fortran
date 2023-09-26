5e438f929305decb7499f0b0bd9a0f55a4804371
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2023-09-26 17:35:39 +0200

    Update main.f90

--- upper_or_lower/main.f90
+++ upper_or_lower/main.f90
@@ -1,37 +1,43 @@
-! ---------------------------------
-! µFortran auto generated program.
-! Created for : ddaddami
-! Date : Tue Sep 26 12:06:23 CEST 2023
-! ---------------------------------
+! ---------------------------------
+
+! µFortran auto generated program.
+
+! Created for : ddaddami
+
+! Date : Tue Sep 26 12:06:23 CEST 2023
+
+! ---------------------------------
+
 program main
-    implicit none
+
+    implicit none
     integer :: mystery, guess
     integer,dimension(8) :: data_time
-    
     call date_and_time(VALUES=data_time)
-    
+
     mystery = int(data_time(8))
-     
-    mystery = mod(mystery*32457,101)
-    
+    random_number = mod(random_number*32457,567)
+    random_number = mod(random_number*25546,249)
+    random_number = mod(random_number*65477,101)
     guess = 101
-    
-    
-        do while(guess<0 .or. guess>100)
-            print*, "Entrez un nombre entre 0 et 100 : "
-            read*, guess
-        enddo
-    
-        if guess == mystery then
-            print*, "Bravo !"
-        elseif guess > mystery then
-            print*, 
-        elseif guess < mystery then
-            print*, 
+
+    do while (guess .ne. mystery)
+
+        print*, "Pick a number from 0 to 100 : "
+        read*, guess
+        
+        if (guess<0 .and. guess>100)then
+            cycle
         endif
-          
-    
-    
-    print*, mystery
-   
-end program main
+        if (guess == mystery) then
+            print*, "Congratulation !"
+            call sleep(5)
+            stop
+        elseif (guess > mystery) then
+            print*, "Lower !"
+        elseif (guess < mystery) then
+        print*, "Higher !"
+        endif
+    end do 
+
+end program main
