2f9ebc1ab462a6943a999cd483e9480244d452ab
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2023-09-29 10:26:59 +0200

    Update main.f90

--- Pi_Monte_Carlo/main.f90
+++ Pi_Monte_Carlo/main.f90
@@ -1,38 +1,88 @@
 ! ---------------------------------
 
+
+
 ! µFortran auto generated program.
 
-! Created for : Diego
 
-! Date : Sat Sep 23 15:21:51 CEST 2023
+
+! Created for : ddaddami
+
+
+
+! Date : Thu Sep 28 16:37:33 CEST 2023
+
+
 
 ! ---------------------------------
 
+
+
 program main
+
+
   implicit none
-  real :: r(10000000, 2)
-  real :: M, d
-  integer :: i, seed_size
-  integer, allocatable :: seed(:)
+  ! Déclaration des variables 
+      integer :: i      ! Entier pour une boucle
+      real :: table(100,2)
+     ! Tableau de stockage des coordonnées de points
+      real :: count, 
+norm      ! Variable utilisée, qui, ont besoins d'être réelles
+      integer, dimension(12) :: seed    ! Déclare un vecteur de dimension 12
 
-  ! seed générée sur l'horloge du système
-  call random_seed(size=seed_size)
-  allocate(seed(seed_size))
-  call random_seed(put=seed)
-  call system_clock(count=seed(1))
-  call random_number(r)
 
-  M = 0
-  do i = 1, size(r, 1)
-    d = sqrt((r(i, 1)**2) + (r(i, 2)**2))
-    if (d <= 1.0) then
-      M = M + 1
-    endif
-  end do
 
-  print *, 4.0 * (M / real(size(r, 1)))
+ 
+  ! Seed pour le random générée sur l'horloge du système
+      call date_and_time(VALUES =seed)
 
-  deallocate(seed)
+
+ ! Donne simplement 
+      call random_seed(put=seed)
+
+
+
+      call random_number(table)
+
+
+  
+  ! Initialisation du compteur et de la boucle principale
+      count = 0
+
+
+ ! Initialisation du compteur
+      do i = 0, size(table, 1)
+
+
+ ! Pour chaque coordonnée dans le tableau
+        norm = sqrt((table(i, 1)**2) + (table(i, 2)**2))
+
+
+ ! On y regarde simplement la norme
+        if (norm <= 1.0) then
+
+
+ ! Condition pour savoir si le points est dans le cercle ou non
+          count = count + 1
+
+
+        endif
+        ! write(*,*) table(i, 1), table(i, 1) si besoin de posséder les coordonées de points
+      end do
+
+
+      
+
+
+
+  ! Simplement écrire dans la console le calcul final du rapport de position
+    print*, 4.0 * (count / real(size(table, 1)))
+
+
+ ! Le real est nécéssaire pour ne pas avoir une division revenue à un entier
+
+
+
 end program main
 
 
