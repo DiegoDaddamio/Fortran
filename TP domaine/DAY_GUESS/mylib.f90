4959f45b8e633760c7a8d99b6c154b402eb67eea
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2023-10-17 12:15:43 +0200

    Add files via upload

---
+++ TP domaine/DAY_GUESS/mylib.f90
@@ -0,0 +1,89 @@
+module mylib
+    contains
+    ! Donne un nombre aléatoire entre 0 et notre _max à UTILISER HORS BOUCLE
+
+    function randint(maximum)
+        integer :: maximum
+        integer, dimension(12) :: seed
+        call date_and_time(VALUES = seed)
+        call random_seed(put = seed)
+        call random_number(randint)
+        randint = randint *1423
+        randint = mod(int(randint),maximum) + 1 
+    end function randint
+
+    
+    subroutine rand_mint(matrix, maximum)
+        integer :: maximum, i, j
+        real:: matrix(:,:)
+        integer, dimension(12) :: seed
+        call date_and_time(VALUES = seed)
+        call random_seed(put = seed)
+        call random_number(matrix)
+        matrix = matrix * 1000
+        matrix = mod(int(matrix), maximum)
+    end subroutine rand_mint
+    ! Donne un nombre aléatoire entre 0 et notre _max_à UTILISER EN BOUCLE
+    function randintloop(maximum,iloop)
+        integer :: maximum
+        integer, dimension(12) :: seed
+        call date_and_time(VALUES =seed)
+        call random_seed(put=seed)
+        call random_number(randintloop)
+        randintloop = randintloop*iloop
+        randintloop = mod(int(randintloop),123) + 1
+        randintloop = randintloop*iloop
+        randintloop = mod(int(randintloop),maximum) + 1
+    end function randintloop
+   
+    ! Construit un fichier ppm ou txt à partir d'une matrice
+    subroutine ppmd(matrix,txt)
+        integer :: matrix(:,:)
+        integer :: lig, col, mx
+        character(*) :: txt
+        character(150) :: format_m
+        lig = size(matrix,1)
+        col = size(matrix,2)
+        mx = maxval(matrix)
+        open(unit=1, file=txt, action="write") 
+        write (1,"(A2)") "P2" 
+        write (1,"(I0.4,x,I0.4)") lig, col
+        write (1,"(I0)") mx
+        call format_matrix_int(matrix,format_m)
+        write (1,format_m) matrix
+        close (unit =1)
+    end subroutine ppmd
+
+    subroutine format_matrix_int(matrix,txt)
+        integer :: matrix(:,:)
+        integer :: m, n, mx, lg
+        character(*) :: txt
+        m = size(matrix, 1)
+        n = size(matrix, 2)
+        write(txt,"(A,I0,A,I0,A)") "(", n, "(", m, "(I0, x),/))"
+    end subroutine format_matrix_int
+    
+    subroutine fractal()
+        complex :: z
+    integer :: k
+    real :: i, j
+    integer, parameter :: norm = 1
+    real, parameter :: prec = 0.005
+    integer, parameter :: inv_prec = int(1/prec)
+    integer,dimension(int(1/norm)*2*inv_prec,int(1/norm)*2*inv_prec) :: matrix = 0
+    iloop : do i = -norm, norm, prec
+        jloop : do j = -norm, norm, prec
+            z = cmplx(real(i),real(j))
+            kloop : do k = 0, 255
+                z = z**2 + cmplx(0.285,0.01) ! {cmplx(-0.2,0.7),cmplx(0.285,0.013),-1,-1.75,...}
+                if (isnan(aimag(z)**2 +real(z)**2) .eqv. .TRUE.) then
+                    matrix(i*inv_prec+int(1/norm)*inv_prec+1,j*inv_prec+int(1/norm)*inv_prec+1) = 255 - k
+                exit
+                endif
+            end do kloop
+        enddo jloop
+    enddo iloop
+call ppmd(matrix,"draw.ppm")
+    end subroutine fractal
+    
+end module mylib
---
+++ TP domaine/DAY_GUESS/obj/mylib.mod
@@ -0,0 +1,64 @@
+GFORTRAN module version '10' created from C:\Users\ddaddami\Desktop\Fortran-main\day_guess/mylib.f90
+MD5:e221e9260931a4bb5dbd82b9e35a06fe -- If you edit this, you'll get what you deserve.
+
+(() () () () () () () () () () () () () () () () () () () () () () () ()
+() () ())
+
+()
+
+()
+
+()
+
+()
+
+()
+
+(2 'format_matrix_int' 'mylib' '' 1 ((PROCEDURE UNKNOWN-INTENT
+MODULE-PROC DECL UNKNOWN 0 0 SUBROUTINE ALWAYS_EXPLICIT) (UNKNOWN 0 0 0
+0 UNKNOWN ()) 3 0 (4 5) () 0 () () () 0 0)
+6 'fractal' 'mylib' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC DECL
+UNKNOWN 0 0 SUBROUTINE) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 0 () () ()
+0 0)
+7 'mylib' 'mylib' '' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 0 () () () 0 0)
+8 'ppmd' 'mylib' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC DECL
+UNKNOWN 0 0 SUBROUTINE ALWAYS_EXPLICIT) (UNKNOWN 0 0 0 0 UNKNOWN ()) 9 0
+(10 11) () 0 () () () 0 0)
+12 'rand_mint' 'mylib' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC DECL
+UNKNOWN 0 0 SUBROUTINE ALWAYS_EXPLICIT) (UNKNOWN 0 0 0 0 UNKNOWN ()) 13
+0 (14 15) () 0 () () () 0 0)
+16 'randint' 'mylib' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC DECL
+UNKNOWN 0 0 FUNCTION) (REAL 4 0 0 0 REAL ()) 17 0 (18) () 16 () () () 0
+0)
+19 'randintloop' 'mylib' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC
+DECL UNKNOWN 0 0 FUNCTION) (REAL 4 0 0 0 REAL ()) 20 0 (21 22) () 19 ()
+() () 0 0)
+4 'matrix' '' '' 3 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0 DIMENSION DUMMY) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (2 0
+ASSUMED_SHAPE (CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '1') () (
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '1') ()) 0 () () () 0 0)
+5 'txt' '' '' 3 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN UNKNOWN 0
+0 DUMMY) (CHARACTER 1 0 0 0 CHARACTER (())) 0 0 () () 0 () () () 0 0)
+10 'matrix' '' '' 9 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0 DIMENSION DUMMY) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (2 0
+ASSUMED_SHAPE (CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '1') () (
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '1') ()) 0 () () () 0 0)
+11 'txt' '' '' 9 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN UNKNOWN
+0 0 DUMMY) (CHARACTER 1 0 0 0 CHARACTER (())) 0 0 () () 0 () () () 0 0)
+14 'matrix' '' '' 13 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0 DIMENSION DUMMY) (REAL 4 0 0 0 REAL ()) 0 0 () (2 0
+ASSUMED_SHAPE (CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '1') () (
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '1') ()) 0 () () () 0 0)
+15 'maximum' '' '' 13 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0 DUMMY) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () () 0 () () () 0 0)
+18 'maximum' '' '' 17 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0 DUMMY) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () () 0 () () () 0 0)
+21 'maximum' '' '' 20 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0 DUMMY) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () () 0 () () () 0 0)
+22 'iloop' '' '' 20 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0 DUMMY) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () () 0 () () () 0 0)
+)
+
+('format_matrix_int' 0 2 'fractal' 0 6 'mylib' 0 7 'ppmd' 0 8 'rand_mint'
+0 12 'randint' 0 16 'randintloop' 0 19)
